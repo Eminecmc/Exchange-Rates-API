a04f6172eb624977e07e930d1734996adbc24dea
Eminecmc <eminecmc08@gmail.com>
2021-04-13 23:52:53 +0300

    exchange

---
+++ Exchange Rates Apı/app.js
@@ -0,0 +1,27 @@
+const amountElement =document.querySelector("#amount");
+const firstSelect = document.querySelector("#firtsCurrency");
+const secondSelect = document.querySelector("#secondCurrency");
+const currency = new Currency("USD","TRY");
+const ui = new UI("USD","TRY");
+eventListeners();
+function eventListeners(){
+    amountElement.addEventListener("input",exchangeCurrency);
+    firstSelect.onchange = function(){
+        currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+        ui.changeFirst();
+    };
+    secondSelect.onchange = function(){
+        currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+        ui.changeSecond();
+    };
+}
+
+function exchangeCurrency(){
+
+    currency.changeAmount(amountElement.value);
+    currency.exchange()
+    .then(result => {
+        ui.displayResult(result);
+    })
+    .catch(err => console.log(err));
+}
